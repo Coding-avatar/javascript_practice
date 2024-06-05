c048a754b9d2c9b4dc967f76e599f3ed80d1fa50
Md Nife Salil Islam <nifesalilislam@gmail.com>
2024-06-05 15:55:48 +0530

    Updated logic, will ignore punctuations, spaces and special characters, updated test cases as well

--- problem_solving/palindrome.js
+++ problem_solving/palindrome.js
@@ -4,9 +4,11 @@
  * @param {string} input - The string to check for palindrome.
  * @returns {boolean} True if the input string is a palindrome, false otherwise.
  */
-function checkPalindrome(input) {
-    const reverseInput = Array.from(input.split('')).reverse().join('');
-    return input.toLowerCase() === reverseInput.toLowerCase();
+function isPalindrome(input) {
+    const filteredInput = input.match(/[a-zA-Z0-9]+/g);
+    const result = filteredInput? filteredInput.join('') : '';
+    const reverseInput = Array.from(result.split('')).reverse().join('');
+    return result.toLowerCase() === reverseInput.toLowerCase();
 }
 
-module.exports = checkPalindrome;
+module.exports = isPalindrome;
