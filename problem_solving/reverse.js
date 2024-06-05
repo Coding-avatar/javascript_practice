8f219d5c5ef7788530bb6fde5a1440784dd4872b
Md Nife Salil Islam <nifesalilislam@gmail.com>
2024-06-05 10:15:03 +0530

    reverse string test added

---
+++ problem_solving/reverse.js
@@ -0,0 +1,15 @@
+/**
+ * Reverses a given string.
+ *
+ * @param {string} word - The input string to reverse.
+ * @returns {string} The reversed string.
+ */
+function reverseString(word) {
+    let reverse = '';
+    for (let i = 0; i < word.length; i++) {
+        reverse = word[i] + reverse;
+    }
+    return reverse;
+}
+
+module.exports = reverseString;
